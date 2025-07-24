2e494be15333caa25f2c45af4f762cf2db3de13d
luis <luis.mch154@gmail.com>
2025-07-24 17:17:02 -0500

    fix: correct typo in column name from 'passsword_hash' to 'password_hash' in User entity

--- src/main/java/com/hopeclinic/api/models/User.java
+++ src/main/java/com/hopeclinic/api/models/User.java
@@ -23,7 +23,7 @@ public class User {
     @NotBlank
     @Length(min = 8)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Column(name = "passsword_hash" , unique = true, length = 255, nullable = false)
+    @Column(name = "password_hash" , unique = true, length = 255, nullable = false)
     private String passwordHash;
 
     @Column(name = "profile_picture_url", length = 255, nullable = true)
