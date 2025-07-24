e641043bc2a891541da30d37e378428107a98022
luis <luis.mch154@gmail.com>
2025-07-24 17:11:11 -0500

    Update SecurityConfig to integrate JWT authentication filter and AuthenticationManager

--- src/main/java/com/hopeclinic/api/config/SecurityConfig.java
+++ src/main/java/com/hopeclinic/api/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.hopeclinic.api.config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -19,6 +28,7 @@ public class SecurityConfig {
         return http.authorizeHttpRequests((auth) ->
             auth.requestMatchers("/api/users").permitAll()
             .anyRequest().authenticated())
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
             .csrf(config -> config.disable())
             .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .build();
